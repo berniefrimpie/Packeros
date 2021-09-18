4aa5f49b2bad9ce1e7d608be884ec02adf064c0d
Bernard Frimpong <bernie.frimpong@outlook.com>
2021-09-18 18:39:29 +0200

    Implement changes to srcfile

--- src/database-connection.js
+++ src/database-connection.js
@@ -3,20 +3,18 @@ const mongoose = require("mongoose");
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DATABASE;
+let connectionString = process.env.MONGODB_CONNECTION_STRING;
+
+if (!connectionString) {
+  connectionString = `mongodb+srv://${username}:${password}@packleros.hbjt2.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+}
 
 mongoose.set("debug", true);
 
 mongoose
-  .connect(
-    `mongodb+srv://${username}:${password}@packleros.hbjt2.mongodb.net/${dbName}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("connection established"))
   .catch(console.log);
-
-// const Panada = mongoose.model("Panda", { name: String, age: Number });
-// Panada.find().then(console.log);
