4ba76e78720f90d94b18334dc2ae6829358f4928
Bernard Frimpong <bernie.frimpong@outlook.com>
2021-09-24 09:59:25 +0200

    Add frontend folder and implement vue.js

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios"
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      users: []
+    }
+  },
+  async created(){
+    const usersRequest = await axios.get("/api/users")
+
+    this.users = usersRequest.data
+
+  }
+};
+</script>
