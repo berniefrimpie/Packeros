25f7d5e5161fc6aa74810a7c0616166fefdb7628
Bernard Frimpong <bernie.frimpong@outlook.com>
2021-10-11 12:19:49 +0200

    Modify users.js file

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const axios = require("axios");
+// const axios = require("axios");
 
 const User = require("../models/user");
 const Photo = require("../models/photo");
